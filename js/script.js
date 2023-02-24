c5541e2d0487ada2842b8be4303afcb030ef68c2
albertseiler <113704524+albertseiler@users.noreply.github.com>
2023-02-24 17:28:22 +0100

    .

--- js/script.js
+++ js/script.js
@@ -1,4 +1,35 @@
-let xBtn = document.querySelector(.closemenu)
+let iconMenu = document.querySelector('.icon-menu')
+let xBtn = document.querySelector('.closemenu')
+let mobileNav = document.querySelector('.nav')
+let body = document.querySelector('body')
+let main = document.querySelector('main')
+let footer = document.querySelector('footer')
+let logoAndBurger = document.querySelector('.logo-and-burger')
+// Add Event listener
 
 
 
+mobileNav.style.visibility = "hidden"
+body.style.backgroundColor = "white"
+body.classList.remove("overlay")
+
+iconMenu.addEventListener("click", (event) => {
+    if (mobileNav.style.visibility = "hidden") {
+        mobileNav.style.visibility = "visible";
+        main.classList.add("blurred");
+        mobileNav.classList.add("no-overlay");
+        footer.style.visibility = 'hidden';
+        logoAndBurger.style.visibility = 'hidden';
+        console.log("Menu opened");
+    }
+})
+
+xBtn.addEventListener("click", (event) => {
+    if (mobileNav.style.visibility = "visible") {
+        mobileNav.style.visibility = "hidden";
+        main.classList.remove("blurred")
+        footer.style.visibility = 'visible';
+        logoAndBurger.style.visibility = 'visible';
+        console.log("Menu closed");
+    }
+})
