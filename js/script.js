74e88ed167d55894c2bb35ec058d364d1478793b
albertseiler <113704524+albertseiler@users.noreply.github.com>
2023-02-27 15:58:43 +0100

    .

--- js/script.js
+++ js/script.js
@@ -5,21 +5,28 @@ let body = document.querySelector('body')
 let main = document.querySelector('main')
 let footer = document.querySelector('footer')
 let logoAndBurger = document.querySelector('.logo-and-burger')
+let featuresBtn = document.querySelector('.featuresbtn')
+let featuresList = document.querySelector('.featureslist')
+let companyBtn = document.querySelector('.companybtn')
+let companyList = document.querySelector('.companylist')
+
 // Add Event listener
 
 
 
 mobileNav.style.visibility = "hidden"
 body.style.backgroundColor = "white"
-body.classList.remove("overlay")
+
+
 
 iconMenu.addEventListener("click", (event) => {
     if (mobileNav.style.visibility = "hidden") {
         mobileNav.style.visibility = "visible";
         main.classList.add("blurred");
         mobileNav.classList.add("no-overlay");
-        footer.style.visibility = 'hidden';
-        logoAndBurger.style.visibility = 'hidden';
+        footer.classList.add("blurred");
+        logoAndBurger.classList.add("blurred");
+        body.style.backgroundColor = "grey";
         console.log("Menu opened");
     }
 })
@@ -27,9 +34,25 @@ iconMenu.addEventListener("click", (event) => {
 xBtn.addEventListener("click", (event) => {
     if (mobileNav.style.visibility = "visible") {
         mobileNav.style.visibility = "hidden";
-        main.classList.remove("blurred")
-        footer.style.visibility = 'visible';
-        logoAndBurger.style.visibility = 'visible';
+        main.classList.remove("blurred");
+        footer.classList.remove("blurred");
+        logoAndBurger.classList.remove("blurred");
+        body.style.backgroundColor = "white";
+        featuresList.classList.remove("openlist");
+        companyList.classList.remove("openlist")
         console.log("Menu closed");
     }
+})
+
+
+featuresBtn.addEventListener("click", (event) => {
+    featuresList.classList.toggle("openlist");
+    console.log("Features list toggled");
+})
+
+
+
+companyBtn.addEventListener("click", (event) => {
+    companyList.classList.toggle("openlist")
+    console.log("Company list toggled")
 })
