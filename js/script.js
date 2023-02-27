70fae5c1702e5bb6e78e65a37b6a32798c3673f5
albertseiler <113704524+albertseiler@users.noreply.github.com>
2023-02-27 16:52:02 +0100

    .

--- js/script.js
+++ js/script.js
@@ -9,6 +9,10 @@ let featuresBtn = document.querySelector('.featuresbtn')
 let featuresList = document.querySelector('.featureslist')
 let companyBtn = document.querySelector('.companybtn')
 let companyList = document.querySelector('.companylist')
+let arrowDownFeatures = document.getElementById('arrowdownfeatures')
+let arrowUpFeatures = document.getElementById('arrowupfeatures')
+let arrowDownCompany = document.getElementById('arrowdowncompany')
+let arrowUpCompany = document.getElementById('arrowupcompany')
 
 // Add Event listener
 
@@ -16,7 +20,10 @@ let companyList = document.querySelector('.companylist')
 
 mobileNav.style.visibility = "hidden"
 body.style.backgroundColor = "white"
-
+arrowDownFeatures.style.display = "inline"
+arrowUpFeatures.style.display = "none"
+arrowDownCompany.style.display = "inline"
+arrowUpCompany.style.display = "none"
 
 
 iconMenu.addEventListener("click", (event) => {
@@ -47,6 +54,11 @@ xBtn.addEventListener("click", (event) => {
 
 featuresBtn.addEventListener("click", (event) => {
     featuresList.classList.toggle("openlist");
+    arrowDownFeatures.style.display == "none" ? arrowDownFeatures.style.display = "inline" :
+        arrowDownFeatures.style.display = "none";
+    arrowUpFeatures.style.display == "inline" ? arrowUpFeatures.style.display = "none" :
+        arrowUpFeatures.style.display = "inline";
+
     console.log("Features list toggled");
 })
 
@@ -54,5 +66,10 @@ featuresBtn.addEventListener("click", (event) => {
 
 companyBtn.addEventListener("click", (event) => {
     companyList.classList.toggle("openlist")
+    arrowDownCompany.style.display == "none" ? arrowDownCompany.style.display = "inline" :
+        arrowDownCompany.style.display = "none";
+    arrowUpCompany.style.display == "inline" ? arrowUpCompany.style.display = "none" :
+        arrowUpCompany.style.display = "inline";
+
     console.log("Company list toggled")
 })
